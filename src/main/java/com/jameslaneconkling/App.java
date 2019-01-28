ae52035077cc669f1b3911516be70087bf4b8c7c
James Conkling <james.lane.conkling@gmail.com>
2019-01-28 13:09:58 -0500

    configure logback + SLF4J logging

--- src/main/java/com/jameslaneconkling/App.java
+++ src/main/java/com/jameslaneconkling/App.java
@@ -1,15 +1,20 @@
 package com.jameslaneconkling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public final class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        logger.info("Hello {}!", "World");
+        logger.debug("Doing {}!", "World");
     }
 }
